72cf96d93a50162b6bddca3b40c0e48925f223dc
Sandeep Khandewale <learner1024@gmail.com>
2017-04-17 23:03:51 +0530

    babel and jquery

--- es6/index.js
+++ es6/index.js
@@ -1,7 +1,7 @@
-$(document).ready(function () {
+$(document).ready(function() {
     $("ul.nav-stacked a").click(function () {
         $("div.pfsection").removeClass("animated fadeIn");
-
+        
         var target = $(this).attr("href");
         $(target).addClass("animated fadeIn");
     });
--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,15 @@
 var gulp = require("gulp");
 var pug = require('gulp-pug');
+var babel = require('gulp-babel');
 
 gulp.task("watch", function(){
   var watcher = gulp.watch('views/*.pug', ['views']);
+});
+
+gulp.task('es6', function(){
+  return gulp.src('es6/*.js')
+    .pipe(babel())
+    .pipe(gulp.dest('js'))
 });
  
 gulp.task('views', function buildHTML() {
@@ -11,4 +18,6 @@ gulp.task('views', function buildHTML() {
   .pipe(gulp.dest('./'))
 });
 
-gulp.task("default", ['views']);
+gulp.task('dist', ['views', 'es6']);
+
+//gulp.task("default", ['views']);
