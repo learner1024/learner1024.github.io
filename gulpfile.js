577174f7a45223c44de9bce08e20bdfd1d1bd166
Sandeep Khandewale <learner1024@gmail.com>
2017-04-17 18:03:53 +0530

    gulp watch and bower.json added

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,7 @@
 var gulp = require("gulp");
 var pug = require('gulp-pug');
+
+var watcher = gulp.watch('views/*.pug', ['views']);
  
 gulp.task('views', function buildHTML() {
   return gulp.src('views/*.pug')
