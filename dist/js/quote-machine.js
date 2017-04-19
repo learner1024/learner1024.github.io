7321928c6b9355d06a3fdda4aab856c53c2268b8
Sandeep Khandewale <learner1024@gmail.com>
2017-04-19 16:53:18 +0530

    quote machine done

---
+++ dist/js/quote-machine.js
@@ -0,0 +1,8 @@
+$(document).ready(function () {
+  $("#genQuote").on("click", function () {
+    $.getJSON("https://crossorigin.me/http://api.forismatic.com/api/1.0/?method=getQuote&format=json&lang=en", function (response) {
+      $("#quoteText").text(response.quoteText);
+      $("#quoteAuthor").text(response.quoteAuthor);
+    });
+  });
+});
