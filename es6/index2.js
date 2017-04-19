b2095801faa3948fbc2afe2260acea274121cf69
Sandeep Khandewale <learner1024@gmail.com>
2017-04-19 16:26:53 +0530

    started to migrate quote machine here from codepen

--- es6/index2.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function() {
-    $("ul.nav-stacked a").click(function () {
-        $("div.pfsection").removeClass("animated fadeIn");
-        
-        var target = $(this).attr("href");
-        $(target).addClass("animated fadeIn");
-    });
-});
