04f5be7a262aacf60541fa116bee786cfd881d7b
Sandeep Khandewale <learner1024@gmail.com>
2017-04-19 14:55:34 +0530

    aahhhaaa major fixes with minor change ;)

--- dist/js/index.js
+++ dist/js/index.js
@@ -1,16 +1,1 @@
-$(document).ready(function () {
-    $("nav ul.nav a").click(function () {
-        $("section").removeClass("animated fadeIn");
-
-        var target = $(this).attr("href");
-        $(target).addClass("animated fadeIn");
-    });
-
-    var showcaseItems = ["portfolio - sandeep khandewale", "tribute - Kishori Amonkar", "random quote machine", "local weather", "wikipedia viewer", "twitch.tv json api", "calc", "pomodoro clock", "tic-tac-toe game", "simon game"];
-    var addToEl = $("#showcase .showcaseItems");
-    showcaseItems.forEach(function (v) {
-        var markup = "\n        <div class=\"col-md-6\">\n            <div class='thumbnail'>                \n                <div class='caption'>\n                    <h2>" + v + " <small> Lorel Ipsum... </small></h2>\n                </div>\n            </div>\n        </div>";
-
-        $(markup).appendTo("#showcase .row#showcaseItems");
-    });
-});
+$(document).ready(function () {});
