0ff7b1c262840b49c4cf8e31e24ea0aa683307bf
Sandeep Khandewale <learner1024@gmail.com>
2017-04-20 11:09:55 +0530

    gulp connect livereload yayayay

--- gulpfile.js
+++ gulpfile.js
@@ -2,34 +2,46 @@ var gulp = require("gulp");
 var pug = require('gulp-pug');
 var babel = require('gulp-babel');
 var sass = require('gulp-sass');
+var connect = require('gulp-connect');
+
+gulp.task('connect', function() {
+  connect.server({
+    livereload: true
+  });
+});
 
 gulp.task("watch", function(){
-  var watcher = gulp.watch([
-    './views/**/*.pug', 
-    './es6/**/*.js', 
-    './sass/**/*.scss'], 
-    ['dist']);
+  gulp.watch(['./views/**/*.pug'],['views']);
+  gulp.watch(['./es6/**/*.js'],['es6']);
+  gulp.watch(['./sass/**/*.scss'],['sass']);
 });
 
 gulp.task('views', function() {
   return gulp.src('views/**/*.pug')
   .pipe(pug())
   .pipe(gulp.dest('./'))
+  .pipe(connect.reload());
 });
 
 gulp.task('es6', function(){
   return gulp.src('es6/**/*.js')
     .pipe(babel())
     .pipe(gulp.dest('./dist/js'))
+    .pipe(connect.reload());
 });
 
 gulp.task('sass', function () {
   return gulp.src('./sass/**/*.scss')
     .pipe(sass())
-    .pipe(gulp.dest('./dist/css'));
+    .pipe(gulp.dest('./dist/css'))
+    .pipe(connect.reload());
 });
 
 gulp.task('dist', ['views', 'es6', 'sass']);
+
+gulp.task('serve', ['connect', 'watch'])
+
+
 
 
 
