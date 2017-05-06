ca94cd95e43e384a154fbb068c599e45a76f76a3
Sandeep Khandewale <learner1024@gmail.com>
2017-05-06 16:40:30 +0530

    switched landing page with new portfolio

--- dist/js/index.js
+++ dist/js/index.js
@@ -1,16 +1,67 @@
 $(document).ready(function () {
-    $("nav ul.nav a").click(function () {
-        $("section").removeClass("animated fadeIn");
+    // $("nav ul.nav a").click(function () {
+    //     // $("section").removeClass("animated fadeIn");
 
-        var target = $(this).attr("href");
-        $(target).addClass("animated fadeIn");
-    });
+    //     // var target = $(this).attr("href");
+    //     // $(target).addClass("animated fadeIn");
+    // });
 
-    var showcaseItems = ["portfolio - sandeep khandewale", "tribute - Kishori Amonkar", "random quote machine", "local weather", "wikipedia viewer", "twitch.tv json api", "calc", "pomodoro clock", "tic-tac-toe game", "simon game"];
-    var addToEl = $("#showcase .showcaseItems");
-    showcaseItems.forEach(function (v) {
-        var markup = "\n        <div class=\"col-md-6\">\n            <div class='thumbnail'>                \n                <div class='caption'>\n                    <h5>" + v + " <small> Lorel Ipsum... </small></h5>\n                </div>\n            </div>\n        </div>";
+    // "My Portfolio", 
+    //     "Tribute to Kishori Amonkar", 
+    //     "Random Quote Machine",
+    //     "Local Weather",
+    //     "wikipedia Viewer",
+    //     "TwitchTV JSON API",
+    //     "Calculator",
+    //     "Pomodoro Clock",
+    //     "Tic Tac Toe Game",
+    //     "Simon Game"
 
-        $(markup).appendTo("#showcase .row#showcaseItems");
+    var showcaseItems = [{
+        title: "My Portfolio (old)",
+        //subtitle: "FreeCodeCamp Basic level front end project",
+        hyperlink: "/index.html"
+    }, {
+        title: "Tribute to Kishori Amonkar",
+        //subtitle: "FreeCodeCamp Basic level front end project",
+        hyperlink: "/tribute-page.html"
+    }, {
+        title: "Random Quote Machine",
+        //subtitle: "int",
+        hyperlink: "/quote-machine.html"
+    }, {
+        title: "Local Weather",
+        //subtitle: "My Portfolio",
+        hyperlink: "/local-weather.html"
+    }, {
+        title: "Wikipedia Viewer",
+        //subtitle: "My Portfolio",
+        hyperlink: "/wiki-viewer.html"
+    }, {
+        title: "TwitchTV JSON API",
+        //subtitle: "My Portfolio",
+        hyperlink: "/twitch-dir.html"
+    }, {
+        title: "Calculator",
+        //subtitle: "My Portfolio",
+        hyperlink: "/js-calc.html"
+    }, {
+        title: "Pomodoro Clock",
+        //subtitle: "My Portfolio",
+        hyperlink: "/pomodoro-clock.html"
+    }, {
+        title: "Tic Tac Toe Game",
+        //subtitle: "My Portfolio",
+        hyperlink: "/ttt.html"
+    }, {
+        title: "Simon Game",
+        //subtitle: "My Portfolio",
+        hyperlink: "/simon.html"
+    }];
+
+    showcaseItems.forEach(function (o) {
+        var markup = "\n        <div class=\"col\">\n            <div class=\"card\">\n                <div class=\"card-block\">\n                    <h4 class=\"card-title\">" + o.title + "</h4>\n                    <!--h6 class=\"card-subtitle mb2 text-muted\">" + o.subtitle + "</h6-->\n                    <p class=\"card-text\">Click <a class=\"card-link\" href=\"" + o.hyperlink + "\" target=\"_blank\">here</a> to visit</p>\n                    \n                </div>\n            </div>\n        </div>";
+
+        $(markup).appendTo("#showcase .row");
     });
 });
--- es6/index.js
+++ es6/index.js
@@ -1,32 +1,89 @@
 $(document).ready(function(){
-    $("nav ul.nav a").click(function () {
-        $("section").removeClass("animated fadeIn");
+    // $("nav ul.nav a").click(function () {
+    //     // $("section").removeClass("animated fadeIn");
         
-        var target = $(this).attr("href");
-        $(target).addClass("animated fadeIn");
-    });
+    //     // var target = $(this).attr("href");
+    //     // $(target).addClass("animated fadeIn");
+    // });
 
-    var showcaseItems = ["portfolio - sandeep khandewale", 
-        "tribute - Kishori Amonkar", 
-        "random quote machine",
-        "local weather",
-        "wikipedia viewer",
-        "twitch.tv json api",
-        "calc",
-        "pomodoro clock",
-        "tic-tac-toe game",
-        "simon game"];
-    var addToEl = $("#showcase .showcaseItems");
-    showcaseItems.forEach(function(v){
+    // "My Portfolio", 
+    //     "Tribute to Kishori Amonkar", 
+    //     "Random Quote Machine",
+    //     "Local Weather",
+    //     "wikipedia Viewer",
+    //     "TwitchTV JSON API",
+    //     "Calculator",
+    //     "Pomodoro Clock",
+    //     "Tic Tac Toe Game",
+    //     "Simon Game"
+
+    var showcaseItems = [
+        {
+            title: "My Portfolio (old)",
+            //subtitle: "FreeCodeCamp Basic level front end project",
+            hyperlink: "/index.html"
+        },
+        {
+            title: "Tribute to Kishori Amonkar",
+            //subtitle: "FreeCodeCamp Basic level front end project",
+            hyperlink: "/tribute-page.html"
+        },
+        {
+            title: "Random Quote Machine",
+            //subtitle: "int",
+            hyperlink: "/quote-machine.html"
+        },
+        {
+            title: "Local Weather",
+            //subtitle: "My Portfolio",
+            hyperlink: "/local-weather.html"
+        },
+        {
+            title: "Wikipedia Viewer",
+            //subtitle: "My Portfolio",
+            hyperlink: "/wiki-viewer.html"
+        },
+        {
+            title: "TwitchTV JSON API",
+            //subtitle: "My Portfolio",
+            hyperlink: "/twitch-dir.html"
+        },
+        {
+            title: "Calculator",
+            //subtitle: "My Portfolio",
+            hyperlink: "/js-calc.html"
+        },
+        {
+            title: "Pomodoro Clock",
+            //subtitle: "My Portfolio",
+            hyperlink: "/pomodoro-clock.html"
+        },
+        {
+            title: "Tic Tac Toe Game",
+            //subtitle: "My Portfolio",
+            hyperlink: "/ttt.html"
+        },
+        {
+            title: "Simon Game",
+            //subtitle: "My Portfolio",
+            hyperlink: "/simon.html"
+        }
+
+    ];
+
+    showcaseItems.forEach(function(o){
         var markup = `
-        <div class="col-md-6">
-            <div class='thumbnail'>                
-                <div class='caption'>
-                    <h5>${v} <small> Lorel Ipsum... </small></h5>
+        <div class="col">
+            <div class="card">
+                <div class="card-block">
+                    <h4 class="card-title">${o.title}</h4>
+                    <!--h6 class="card-subtitle mb2 text-muted">${o.subtitle}</h6-->
+                    <p class="card-text">Click <a class="card-link" href="${o.hyperlink}" target="_blank">here</a> to visit</p>
+                    
                 </div>
             </div>
         </div>`;
 
-        $(markup).appendTo("#showcase .row#showcaseItems");
+        $(markup).appendTo("#showcase .row");
     })
 })
